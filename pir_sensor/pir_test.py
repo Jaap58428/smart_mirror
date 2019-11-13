09d3377371e9212f4f0ee2dc2ebd9f4b1dc804ee
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-11-13 14:37:32 +0100

    added pulldown resistor

--- pir_sensor/pir_test.py
+++ pir_sensor/pir_test.py
@@ -1,18 +1,18 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 pir_sensor = 7
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pir_sensor, GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pir_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 current_state = 0
 
 try:
     while True:
-        time.sleep(0.2)
         current_state = GPIO.input(pir_sensor)
         print(current_state)
+        sleep(0.1)
 except KeyboardInterrupt:
     pass
 finally:
