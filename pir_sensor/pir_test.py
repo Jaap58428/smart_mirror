01ee51f00f697f39c433d94b354937ed31267449
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-11-13 12:58:40 +0100

    fixed tabs

--- pir_sensor/pir_test.py
+++ pir_sensor/pir_test.py
@@ -6,13 +6,10 @@ GPIO.setmode(GPIO.BOARD)
 current_state = 0
 
 for i in range(20):
-
-
     try:
         time.sleep(0.5)
-	GPIO.setup(pir_sensor, GPIO.IN)
-	    current_state = GPIO.input(i)
-	    print(current_state, i)
-	except KeyboardInterrupt:
-	    pass
-
+        GPIO.setup(pir_sensor, GPIO.IN)
+        current_state = GPIO.input(i)
+        print(current_state, i)
+    except Exception:
+        pass
