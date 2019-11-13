27fd1ab96ffabafbcdcf8943b4f93b359cbe6f02
Toastje123# <jaapkanbier@hotmail.com>
2019-11-13 11:54:47 +0000

    pir 1.2

--- pir_sensor/pir_test.py
+++ pir_sensor/pir_test.py
@@ -1,22 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-pir_sensor = 11
-
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(pir_sensor, GPIO.IN)
-
 current_state = 0
-try:
-    while True:
-        time.sleep(0.1)
-        current_state = GPIO.input(pir_sensor)
-        if current_state == 1:
-            print("Motion detected!")
-        else:
-            print("No motion detected :(")
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+
+for i in range(20):
+
+
+    try:
+        time.sleep(0.5)
+	GPIO.setup(pir_sensor, GPIO.IN)
+	    current_state = GPIO.input(i)
+	    print(current_state, i)
+	except KeyboardInterrupt:
+	    pass
+
