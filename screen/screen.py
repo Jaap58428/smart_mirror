608c1610a3dbde9ae6991a676202d8c5af2f37bf
Toastje123# <jaapkanbier@hotmail.com>
2019-11-14 15:57:17 +0100

    use newlines instead of tabs

--- screen/screen.py
+++ screen/screen.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 
 window = Tk()
 window.geometry("600x400")
-window.title("tkinter!")
+window.title("Smart mirror")
 
 lbl = Label(window, text="EMPTY", )
 lbl.pack()
@@ -17,21 +17,31 @@ PIR_SENSOR = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIR_SENSOR, GPIO.IN)
 
-print("Bootin device")
-sleep(15)
+print("Booting device...")
+for i in reversed(range(15)):
+	print("[*] {} [*]".format(i))
+	sleep(1)
+
+humidity = 0
+temperature = 0
+motion = 0
 
 while(True):
-	s = 'no motion detected' if not GPIO.input(PIR_SENSOR) else 'MOTION DETECTED'
-
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 17)
-
-	if humidity is None or temperature is None:
+	new_motion = GPIO.input(PIR_SENSOR)
+	new_humidity, new_temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 17)
+	
+	new_humidity = round(new_humidity, 2)
+	new_temperature = round(new_temperature, 2)
+	if new_humidity == humidity and new_temperature == temperature and new_motion == motion:
 		continue
 	
-	humidity = round(humidity, 2)
-	temperature = round(temperature, 2)
-		
-	lbl.configure(text="Humidity = {}\t temperature = {}\t {}".format(humidity, temperature, s))
+	humidity = new_humidity
+	temperature = new_temperature
+	motion = new_motion
+
+	s = 'no motion detected' if not motion else 'MOTION DETECTED'
+	
+	lbl.configure(text="Humidity = {}\n Temperature = {}\n Motion = {}".format(humidity, temperature, s))
 		
 	window.update_idletasks()
 	window.update()
