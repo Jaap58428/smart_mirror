188571eb02799b40cd1125188f0fb8fde65b1c01
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-11-18 22:14:34 +0100

    changed GUI to have relative sizes

--- screen/test.py
+++ screen/test.py
@@ -4,58 +4,59 @@ import random
 
 colors = {
     "BACKGROUND": "#000",
-    "BANNER": "#0f0",
-    "HEAT_IMAGE": "#f00"
+    "FONT_COLOR": "#fff",
+    "HEAT_IMAGE": "#f00",
+    "DATA_PANEL": "#00f"
 }
 
 
 def get_main_window():
     window = tk.Tk()
     window.title("Smart Mirror GUI")
-    window.configure(bg=colors["BACKGROUND"])
+    window.configure(
+        bg=colors["BACKGROUND"],
+    )
+    window.geometry('600x1000')
+    window.minsize(150, 100)
     return window
 
 
-def get_banner(parent):
-    banner = tk.Frame(parent)
-    banner.configure(
-        bg=colors["BANNER"],
-        bd=0,
-        height=600,
-        width=800,
-    )
-    banner.pack(side=tk.TOP)
-    return banner
-
-
 def get_heat_image_panel(parent):
-    heat_image_panel = tk.Frame(parent, bg=colors["HEAT_IMAGE"])
-    heat_image_panel.configure(height="300", width="500")
-    banner.pack()
+    heat_image_panel = tk.Frame(parent)
+    heat_image_panel.configure(
+        bg=colors["HEAT_IMAGE"],
+    )
+    heat_image_panel.place(relwidth=0.2, relheight=0.2, anchor=tk.NW)
+
     return heat_image_panel
 
 
-def generate_data_labels(banner, data_set):
-
+def generate_data_labels(parent, data_set):
     string_pointers = {}
-
     for attr, val in data_set.items():
         string_pointers[attr] = tk.StringVar()
         string_pointers[attr].set('{0}: {1}'.format(attr, str(val)))
-        new_label = tk.Label(banner, textvariable=string_pointers[attr])
+        new_label = tk.Label(parent)
+        new_label.configure(
+            textvariable=string_pointers[attr],
+            bg=colors["BACKGROUND"],
+            fg=colors["FONT_COLOR"],
+            anchor=tk.E
+        )
         new_label.pack()
-
-    return banner, string_pointers
+    return parent, string_pointers
 
 
 def get_data_panel(parent, data_set):
-    data_panel = tk.Frame(parent, bg=colors["HEAT_IMAGE"])
-    data_panel.configure(height="300", width="500")
-    banner.pack()
+    data_panel = tk.Frame(parent)
+    data_panel.configure(
+        bg=colors["DATA_PANEL"],
+    )
+    data_panel.place(relwidth=0.3, relheight=0.2, anchor=tk.NE)
 
-    filled_banner, string_pointers = generate_data_labels(banner, data_set)
+    updated_data_panel, string_pointers = generate_data_labels(parent, data_set)
 
-    return data_panel, string_pointers
+    return updated_data_panel, string_pointers
 
 
 def get_mock_data():
@@ -67,26 +68,24 @@ def get_mock_data():
 
 def update_string_pointers(string_pointers, data_set):
     for attr, pointer in string_pointers.items():
-        pointer.set(data_set[attr])
+        pointer.set('{0}: {1}'.format(attr, data_set[attr]))
 
 
 if __name__ == '__main__':
     data_set = get_mock_data()
 
     window = get_main_window()
-    banner = get_banner(window)
 
-    heat_image_panel = get_heat_image_panel(banner)
-    data_panel, string_pointers = get_data_panel(banner, data_set)
+    heat_image_panel = get_heat_image_panel(window)
+    data_panel, string_pointers = get_data_panel(window, data_set)
 
     while(True):
         data_set = get_mock_data()
 
         update_string_pointers(string_pointers, data_set)
 
-        # string_pointers['temp'].set(i)
         window.update_idletasks()
         window.update()
 
-        sleep(1)
-        # sleep(0.033)  # equals to around 30fps
+        sleep(0.1)
+        sleep(0.033)  # equals to around 30fps
