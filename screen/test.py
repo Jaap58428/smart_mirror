f6ea5ec7095953518fac885f9e04c4bf385eb942
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-11-19 11:04:59 +0100

    color enum and added TODO's

--- screen/test.py
+++ screen/test.py
@@ -1,32 +1,48 @@
 import tkinter as tk
 from time import sleep
 import random
+from enum import Enum
 
-colors = {
-    "BACKGROUND": "#000",
-    "FONT_COLOR": "#fff",
-    "HEAT_IMAGE": "#f00",
-    "DATA_PANEL": "#00f"
-}
+
+class Color(Enum):
+    BACKGROUND = "#000"
+    FONT_COLOR = "#FFF"
+    HEAT_PANEL = "#F00"
+    DATA_PANEL = "#0F0"
 
 
 def get_main_window():
     window = tk.Tk()
     window.title("Smart Mirror GUI")
     window.configure(
-        bg=colors["BACKGROUND"],
+        bg=Color.BACKGROUND.value
     )
     window.geometry('600x1000')
+
+    # TODO: fill full screen
     window.minsize(150, 100)
+    window.wm_state("zoomed")
+
+    # window.bind("<F11>", window.fullscreen_toggle)
+    # window.bind("<Escape>", window.fullscreen_cancel)
+    #
+    # self.parent.focus_set()
+    # self.parent.overrideredirect(True)
+    #
+    # self.parent.attributes("-fullscreen", True)
+    # self.parent.wm_attributes("-topmost", 1)
+
     return window
 
 
 def get_heat_image_panel(parent):
     heat_image_panel = tk.Frame(parent)
     heat_image_panel.configure(
-        bg=colors["HEAT_IMAGE"],
+        bg=Color.HEAT_PANEL.value,
     )
     heat_image_panel.place(relwidth=0.2, relheight=0.2, anchor=tk.NW)
+
+    # TODO add video stream
 
     return heat_image_panel
 
@@ -39,9 +55,10 @@ def generate_data_labels(parent, data_set):
         new_label = tk.Label(parent)
         new_label.configure(
             textvariable=string_pointers[attr],
-            bg=colors["BACKGROUND"],
-            fg=colors["FONT_COLOR"],
-            anchor=tk.E
+            bg=Color.BACKGROUND.value,
+            fg=Color.FONT_COLOR.value,
+            anchor=tk.E,
+            font=("default", 120)
         )
         new_label.pack()
     return parent, string_pointers
@@ -50,7 +67,7 @@ def generate_data_labels(parent, data_set):
 def get_data_panel(parent, data_set):
     data_panel = tk.Frame(parent)
     data_panel.configure(
-        bg=colors["DATA_PANEL"],
+        bg=Color.DATA_PANEL.value,
     )
     data_panel.place(relwidth=0.3, relheight=0.2, anchor=tk.NE)
 
@@ -87,5 +104,5 @@ if __name__ == '__main__':
         window.update_idletasks()
         window.update()
 
-        sleep(0.1)
+        # sleep(0.1)
         sleep(0.033)  # equals to around 30fps
