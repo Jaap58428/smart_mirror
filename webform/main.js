6b75e117e2d41cba9db62b1b45aaf965eadbac94
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-11-28 17:21:17 +0100

    webform displays attributes and values

--- webform/main.js
+++ webform/main.js
@@ -1,9 +1,56 @@
-const buildFormElement = () => {
-    // Create label
-    // Create input
-    // Define input type: boolean, integer, float
-    // https://www.w3schools.com/tags/tag_input.asp
-    // Set value to current value
+const customSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+
+    let results = event.target
+
+    for (const result in results) {
+        if (results.hasOwnProperty(result)) {
+            const element = results[result];
+            console.log(element.value);
+            
+        }
+    }
+}
+
+const buildFormElement = (attribute, value, formElementType) => {
+    let label = document.createElement("label")
+    label.setAttribute("for", attribute)
+    label.innerHTML = attribute
+
+    let input = document.createElement("input")
+    input.setAttribute("name", attribute)
+    input.setAttribute("id", attribute)
+
+    label.appendChild(input)
+
+    switch (formElementType) {
+        case "boolean":
+            input.setAttribute("type", "checkbox")
+            if (value) {
+                input.setAttribute("checked", true)
+            }
+
+            break;
+
+        case "integer":
+        case "float":
+            input.setAttribute("type", "number")
+            input.setAttribute("min", 0)
+            input.setAttribute("max", 120)
+            input.value = value
+            break;
+
+        default:
+            input.setAttribute("type", "text")
+            break;
+    }
+
+    let inputbox = document.createElement("div")
+    inputbox.classList.add("formInputBox")
+    inputbox.appendChild(label)
+
+    document.getElementById("form").appendChild(inputbox)
     // Add element to DOM
 
 }
@@ -27,7 +74,7 @@ const getFormElementType = (attribute) => {
             formElementType = "float"
 
         default:
-            console.warn("Invalid form element type!", configElement);
+            console.warn("Invalid form element type!", attribute);
             break;
     }
 
@@ -47,8 +94,18 @@ const loadConfigToForm = (response) => {
     }
 
     if (Object.keys(configObject).length > 0) {
-        console.warn("ADD SAVE BUTTON WHEN THERE IS AN ATTRIBUTE TO EDIT");
-        
+        let submitButton = document.createElement("button")
+        submitButton.classList.add("submitbutton")
+        submitButton.innerHTML = "Save"
+
+        let inputbox = document.createElement("div")
+        inputbox.classList.add("formInputBox")
+        inputbox.appendChild(submitButton)
+
+        let form = document.getElementById("form")
+
+        form.appendChild(inputbox)
+        form.onsubmit = customSubmit
     } else {
         console.warn("No attributes are returned to edit!");
     }
@@ -67,12 +124,25 @@ const getCurrentConfig = (configGetUrl) => {
     };
     xhttp.open("GET", configGetUrl, true);
     xhttp.send();
-    console.log("Fetching config from Smart Mirror...");
+    console.log("Fetching config from Smart Mirror...", configGetUrl);
+
+
+    // PURE FOR DEBUG PURPOSES, REMOVE LATER
+    response = `{
+        "use_humidity": false,
+        "display_host_ip": true,
+        "display_sleep_timer": false,
+        "display_debug_panel": false,
+        "sleep_timeout_sec": 10,
+        "screen_max_frame_rate": 0.033,
+        "ambient_temp_delay": 2,
+    }`
+    loadConfigToForm(response)
 }
 
 const main = () => {
     console.log("Page has loaded");
-    const configGetUrl = "/config"
+    const configGetUrl = "./mock_response.json"
     getCurrentConfig(configGetUrl)
 }
 
