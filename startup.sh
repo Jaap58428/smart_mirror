cfa0c79738b7f0e828be7472284d17c2a530753a
Dodo <kasper199914@gmail.com>
2019-12-05 12:15:06 +0100

    use bash instead of sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo python3 /home/jaap/smart_mirror/main.py >> log.txt
-/home/ghost/.cargo/watchexec -w /home/jaap/smart_mirror/rocket /bin/sh reboot.sh
+/home/ghost/.cargo/watchexec -w /home/jaap/smart_mirror/rocket /bin/bash /home/jaap/smart_mirror/reboot.sh
