c91bc4f15a04fb2c181aee4cea50751800cbf922
DutchGhost <root@raspberrypi>
2019-12-06 13:05:32 +0100

    Latest WIP

--- reboot.sh
+++ reboot.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-pgrep python | sudo xargs kill
+PYPID="$(pgrep python)"
+
+echo $PYPID
+if [ $PYPID ]
+then
+	pgrep python | sudo xargs kill;
+fi
 sudo python3 /home/ghost/smart_mirror/main.py >> log.txt &
--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo python3 /home/ghost/smart_mirror/main.py >> log.txt &
-/home/ghost/.cargo/bin/watchexec -w /home/ghost/smart_mirror/rocket /bin/bash /home/ghost/smart_mirror/reboot.sh &
+
