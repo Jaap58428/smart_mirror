2e6a51bb863ba8392a79aac4585ebbbb0b5b9600
Jaap Kanbier <jaapkanbier@hotmail.com>
2020-01-22 15:26:23 +0100

    restored sensor.py and removed echoes from startup bash

--- start_mirror.sh
+++ start_mirror.sh
@@ -3,10 +3,7 @@
 echo $0
 
 full_path=$(realpath $0)
-echo $full_path
-
 dir_path=$(dirname $full_path)
-echo $dir_path
 
 sudo $dirpath/rocket/target/release/rocket &
 sudo python3 $dir_path/main.py &
